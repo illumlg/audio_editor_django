95b8776612cf5c08cfb6826643ed3f056f2e85a7
Illuminati <scrall1008@gmail.com>
2020-12-10 13:31:25 +0200

    minor fixes

--- audio_editor/const.py
+++ audio_editor/const.py
@@ -1,7 +1,9 @@
+from audio_editor_django.settings import BASE_DIR
+
 DATABASE = 'sqlite3.db'
 VALID_FORMATS = ['.aif', '.aifc', '.aiff', '.au', '.avr', '.snd', '.wav']
-INPUT_DIRECTORY = 'input files/'
-OUTPUT_DIRECTORY = 'output files/'
+INPUT_DIRECTORY = BASE_DIR / 'input files/'
+OUTPUT_DIRECTORY = BASE_DIR / 'output files/'
 CONTENT_TYPE = ['audio']
 MAX_CONTENT_SIZE = 20 * 1024 * 1024  # 20Mb max limit.
 STORAGE_LIMIT = 400 * 1024 * 1024 # 400Mb
